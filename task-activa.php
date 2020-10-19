8cea2232c3d3a8fd58690ad1c17fbf8ba354d5d5
Miguel Osorio Naranjo <miguelosorionaranjo@gmail.com>
2020-10-19 10:15:45 -0500

    activar/desactivar

--- task-activa.php
+++ task-activa.php
@@ -2,7 +2,7 @@
   include('database.php');
 if(isset($_POST['id'])) { 
   $id = $_POST['id'];
-  $query = "UPDATE task SET  activa=IF(activa='No Aprobado', 'Aprobado', 'No Aprobado')  WHERE id = '$id'";
+  $query = "UPDATE task SET activa = if(activa='No Aprobado', 'Aprobado', 'No Aprobado') WHERE id = '$id'";
   $result = mysqli_query($connection, $query);
   if (!$result) {
     die('Consulta Fallida.');
